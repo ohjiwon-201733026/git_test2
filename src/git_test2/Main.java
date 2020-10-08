0bf09474d00616ae8db3ed2525eced7d75161b42
Oh,Jiwon <ojw970725@gmail.com>
2020-10-09 02:01:49 +0900

    화면 출력 방법 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hello hello=new Hello();
-		hello.say();
+		system.out.println(hello.say());
 	}
 
 }
